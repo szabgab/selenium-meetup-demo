d2d6a3746780087f4ae9b8aab227e91bcf1a8324
Gabor Szabo <gabor@szabgab.com>
2021-06-17 10:49:11 +0300

    writing our first demo test

--- README.md
+++ README.md
@@ -5,3 +5,26 @@
 1. Implement Tests
 1. Deploy Tests to Github Action [See](https://www.tocode.co.il/blog/2020-03-python-selenium-github-actions)
 
+# Test Plan
+
+## Homepage Layout - What Should Be In The Homepage
+
+### Test #1
+We expect to see in the homepage:
+    - banner text
+    - boxes with images and meetup category names
+    - join meetup button
+    - top header login/sign up
+    - search box
+
+### Test #2
+Verify You can click on a Category from the homepage and get to its search result page
+
+### Test #3
+Verify Gabor can login
+
+### Test #4
+Search for a hike in UK
+Verify you can search from the homepage for what you want and get to the result page
+
+
--- tests/test_access.py
+++ tests/test_access.py
@@ -1,6 +1,20 @@
-from selenium import webdriver
 
-def test_main_page(browser):
-    browser.get("https://www.meetup.com/")
-    print(browser.title)
+meetup_homepage_url = "https://www.meetup.com/"
+import pdb
+
+def test_title(browser):
+    browser.get(meetup_homepage_url)
     assert browser.title == "Meetup - We are what we do"
+
+    # - banner text
+    # - boxes with images and meetup category names
+    # - join meetup button
+    # - top header login/sign up
+    # - search box
+def test_homepage(browser):
+    browser.get(meetup_homepage_url)
+    headers = browser.find_elements_by_css_selector('main h1')
+    assert len(headers) == 1
+
+    header = headers[0]
+    assert len(header.text) > 2
