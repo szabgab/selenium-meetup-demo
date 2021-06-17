45222a061948220dc1a8deb9765807042deb1ab3
Gabor Szabo <gabor@szabgab.com>
2021-06-17 11:54:58 +0300

    add test case for searching form

--- tests/test_access.py
+++ tests/test_access.py
@@ -1,3 +1,5 @@
+from selenium.webdriver.common.keys import Keys 
+
 
 meetup_homepage_url = "https://www.meetup.com/"
 import pdb
@@ -34,3 +36,29 @@ def test_homepage(browser):
 
         assert image_was_loaded is True, img.get_attribute('src')
 
+    forms = browser.find_elements_by_tag_name("form")
+    #assert len(forms) == 1
+    # TODO: if there are more forms print them out
+    # TODO: a better way to locate the form using an id (that it does not have now)
+    # len(forms) == 1
+    form = forms[0]
+    assert form.find_element_by_id("search-keyword-input")
+    assert form.find_element_by_id("location-typeahead-searchLocation")
+    assert form.find_element_by_css_selector('[data-testid="search-submit"]')
+
+def test_search_form(browser):
+    browser.get(meetup_homepage_url)
+
+    form = browser.find_element_by_tag_name("form")
+    form.find_element_by_id("search-keyword-input").send_keys("hiking")
+    form.find_element_by_id("location-typeahead-searchLocation").send_keys("London, GB") 
+    # TODO: make the location selector work, try this:
+    # https://stackoverflow.com/questions/28482297/send-keys-not-to-element-but-in-general-selenium
+    #form.find_element_by_id("location-typeahead-searchLocation").send_keys(Keys.DOWN)
+    form.find_element_by_id("location-typeahead-searchLocation").send_keys(Keys.ENTER)
+    assert browser.title == 'Find Events & Groups | Meetup'
+
+    # import code
+    # code.interact(local=locals())
+    #pdb.set_trace()
+    
